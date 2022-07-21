134e62017707af3006819feb0160192899038383
JustSong <quanpengsong@gmail.com>
2022-07-22 00:47:54 +0800

    :tada: update test

--- main.cpp
+++ main.cpp
@@ -6,7 +6,13 @@ using namespace std;
 
 void getIndexPage(const HTTPRequestPtr &req, const HTTPConnectionPtr &res) {
     LOG_INFO("getIndexPage");
-    string html = "<h1>Hello World!</h1>";
+    string html = "<h1>Hello World!</h1><a href='/echo'>Go To Echo Page</a>";
+    res->send(html);
+}
+
+void getEchoPage(const HTTPRequestPtr &req, const HTTPConnectionPtr &res) {
+    LOG_INFO("getEchoPage");
+    string html = "<li> Method: " + req->method + "</li><li> URL: " + req->url + "</li>";
     res->send(html);
 }
 
@@ -14,6 +20,7 @@ int main() {
     EventLoop loop;
     HTTPServer server(&loop, 3000, 4);
     server.setRoute("/", getIndexPage);
+    server.setRoute("/echo", getEchoPage);
     server.start();
     loop.loop();
     return 0;
